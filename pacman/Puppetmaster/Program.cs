e6439678a8c080ee585313614e9286ccda03de5c
DESKTOP-BFMQIE6\Andre <andrebnribeiro@hotmail.com>
2017-12-06 13:34:28 +0000

    puppetmaster v1.0

---
+++ pacman/Puppetmaster/Program.cs
@@ -0,0 +1,22 @@
+﻿using Proxy;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Threading;
+using System.Timers;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puppetmaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
